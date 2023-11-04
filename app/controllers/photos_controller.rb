72faee035e62217b10c911636f2f9e4e91f1fc98
smenon2003 <146893219+smenon2003@users.noreply.github.com>
2023-11-04 19:27:24 +0000

    added delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,21 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc})
     render({ :template => "photo_templates/index"})
   end 
+
+  def show
+    # Parameters: {"path_id=>"777"}
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id})
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show"})
+  end 
+
+  def baii
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+    # render({ :template => "photo_templates/baii"})
+    redirect_to("/photos")
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:path_username", { :controller => "users", :action => "show"})
   get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
 end
