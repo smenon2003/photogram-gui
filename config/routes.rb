373747816337664b45557e31fc8d638575446f16
smenon2003 <146893219+smenon2003@users.noreply.github.com>
2023-11-04 20:36:34 +0000

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get("/photos/:path_id", { :controller => "photos", :action => "show"})
   get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
   get("/insert_photo", { :controller => "photos", :action => "create"})
+  get("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
+  get("/add_comment/:add_id", { :controller => "photos", :action => "add_comment"})
 end
